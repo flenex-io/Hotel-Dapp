a0eecac12f84c9cae3984c28ef323c9f6e39d9d8
Jagjot Singh <dev.flenex@gmail.com>
2023-12-14 22:00:46 +0530

    Updated jsx

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Main.css";
 const ethers = require("ethers");
 
@@ -6,11 +6,7 @@ const Main = ({ state }) => {
   const { ethereum } = window;
   const [roomCount, setRoomCount] = useState(null);
   const [booked, setBooked] = useState(false);
-
-  useEffect(() => {
-    fetchRoomCount();
-    checkBookedRoom();
-  }, []);
+  const [showRoomCount, setShowRoomCount] = useState(false);
 
   const handleBookingError = (error) => {
     alert("Error booking");
@@ -67,15 +63,21 @@ const Main = ({ state }) => {
     }
   };
 
+  const handleFetchRoomCount = () => {
+    setShowRoomCount(true);
+    fetchRoomCount();
+  };
+
+
   return (
-    <div className="main-container">
+    <>
       <nav className="nav-bar">
         <h3>ETH ROOMS</h3>
         <div className="nav-buttons">
           <button className="nav-button" onClick={book}>
             Book Now
           </button>
-          <button className="nav-button" onClick={fetchRoomCount}>
+          <button className="nav-button" onClick={handleFetchRoomCount}>
             Check Room Availability
           </button>
           <button className="nav-button" onClick={checkBookedRoom}>
@@ -86,22 +88,28 @@ const Main = ({ state }) => {
           </button>
         </div>
       </nav>
-      {roomCount !== null && (
-        <p className="room-count">Room Count: {roomCount} available</p>
+      {showRoomCount && roomCount !== null && (
+        <p className="room-count">{roomCount} rooms available</p>
       )}
       <p className="booked-status">{booked}</p>
-      <p>
-        Aute ea veniam consectetur tempor sunt do sint culpa adipisicing
-        occaecat mollit minim elit. Sunt sint proident tempor <br />
-        sit veniam dolor. Adipisicing aliqua reprehenderit adipisicing ex tempor
-        qui esse eiusmod elit eu esse ad est. Nostrud ex ipsum velit id occaecat{" "}
-        <br /> mollit esse adipisicing velit culpa. Eiusmod fugiat Lorem
-        proident do esse laboris sit incididunt est ipsum nulla exercitation.
-        Duis exercitation ex id duis qui esse. <br />
-        Proident excepteur laborum exercitation sint consectetur irure
-        adipisicing incididunt tempor.
-      </p>
-    </div>
+      <div className="content">
+        <p>
+          Aute ea veniam consectetur tempor sunt do sint culpa adipisicing
+          occaecat mollit minim elit. Sunt sint proident tempor <br />
+          sit veniam dolor. Adipisicing aliqua reprehenderit adipisicing ex
+          tempor qui esse eiusmod elit eu esse ad est. Nostrud ex ipsum velit id
+          occaecat <br /> mollit esse adipisicing velit culpa. Eiusmod fugiat
+          Lorem proident do esse laboris sit incididunt est ipsum nulla
+          exercitation. Duis exercitation ex id duis qui esse. <br />
+          Proident excepteur laborum exercitation sint consectetur irure
+          adipisicing incididunt tempor.
+        </p>
+        <div className="section">
+          <h4>Section Title</h4>
+          <p>This is a section of content within your layout.</p>
+        </div>
+      </div>
+    </>
   );
 };
 
